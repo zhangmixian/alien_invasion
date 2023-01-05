e577078abd516ed3a914606b3d1b0ce169e78c34
zmx <zhangmixian@gmail.com>
2023-01-05 20:10:45 +0800

    重新格式化了一下

--- game_functions.py
+++ game_functions.py
@@ -1,10 +1,11 @@
 import sys
+
 import pygame
 
 
 def check_event():
     for event in pygame.event.get():
-        if event.type== pygame.QUIT:
+        if event.type == pygame.QUIT:
             sys.exit()
 
 
