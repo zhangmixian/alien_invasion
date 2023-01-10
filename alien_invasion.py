0c30d40199527994a27d3262952548b4afd59ffa
zmx <zhangmixian@gmail.com>
2023-01-10 21:39:57 +0800

    fire!!!

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,20 +3,25 @@ import pygame
 import game_functions
 from settings import Settings
 from ship import Ship
-
+from pygame.sprite import Group
 
 def run_game():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     ai_ship = Ship(screen,ai_settings)
+    bullets=Group()
 
     pygame.display.set_caption("Alien Incision")
     ai_settings.bg_color = (230, 230, 230)
 
     while True:
-        game_functions.check_event(ai_ship)
-        game_functions.update_screen(screen, ai_settings, ai_ship)
+        game_functions.check_event(ai_ship,screen,bullets,ai_settings)
+        ai_ship.update()
+        game_functions.update_bullets(bullets)
+        game_functions.update_screen(screen, ai_settings, ai_ship,bullets)
+
+
 
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -2,31 +2,51 @@ import sys
 
 import pygame
 
+from bullet import Bullet
 
-def check_event_down(event,ai_ship):
-        if event.key == pygame.K_RIGHT:
-            ai_ship.moving_right = True
-        if event.key == pygame.K_LEFT:
-            ai_ship.moving_left = True
 
-def check_event_up(event,ai_ship):
-        if event.key == pygame.K_RIGHT:
-            ai_ship.moving_right = False
-        if event.key == pygame.K_LEFT:
-            ai_ship.moving_left = False
+def fire_bullet(ai_ship, bullet, ai_settings, screen):
+    if len(bullet) < ai_settings.bullet_allow:
+        new_bullet = Bullet(ai_settings, screen, ai_ship)
+        bullet.add(new_bullet)
+def check_event_down(event, ai_ship, bullet, ai_settings, screen):
+    if event.key == pygame.K_RIGHT:
+        ai_ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ai_ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        fire_bullet(ai_ship, bullet, ai_settings, screen)
 
-def check_event(ai_ship):
+def check_event_up(event, ai_ship):
+    if event.key == pygame.K_RIGHT:
+        ai_ship.moving_right = False
+    if event.key == pygame.K_LEFT:
+        ai_ship.moving_left = False
+
+
+def check_event(ai_ship, screen, bullet, ai_settings):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_event_down(event,ai_ship)
+            check_event_down(event, ai_ship, bullet, ai_settings, screen)
         elif event.type == pygame.KEYUP:
-            check_event_up(event,ai_ship)
+            check_event_up(event, ai_ship)
 
-def update_screen(screen, ai_settings, ai_ship):
+
+def update_screen(screen, ai_settings, ai_ship, bullet):
     screen.fill(ai_settings.bg_color)
-    ai_ship.update()
+    bullet.update()
+
     ai_ship.blitme()
+    for bullets in bullet.sprites():
+        bullets.draw_bullet()
 
     pygame.display.flip()
+
+
+def update_bullets(bullets):
+    bullets.update()
+    for bullet in bullets:
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
